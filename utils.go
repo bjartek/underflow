faec52155fef887ed5cec905871259bba9f83492
Bjarte Stien Karlsen <bjarte@bjartek.org>
2024-02-15 13:43:03 +0100

    added code from overflow

---
+++ utils.go
@@ -0,0 +1,176 @@
+package underflow
+
+import (
+	"bufio"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"math"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/araddon/dateparse"
+	"github.com/onflow/cadence"
+)
+
+// go string to cadence string panic if error
+func cadenceString(input string) cadence.String {
+	value, err := cadence.NewString(input)
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
+
+func parseTime(timeString string, location string) (string, error) {
+	loc, err := time.LoadLocation(location)
+	if err != nil {
+		return "", err
+	}
+	time.Local = loc
+	t, err := dateparse.ParseLocal(timeString)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%d.0", t.Unix()), nil
+}
+
+func getAndUnquoteString(value cadence.Value) string {
+	result, err := strconv.Unquote(value.String())
+	if err != nil {
+		result = value.String()
+		if strings.Contains(result, "\\u") || strings.Contains(result, "\\U") {
+			result = value.ToGoValue().(string)
+		}
+	}
+
+	return strings.Replace(result, "\x00", "", -1)
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+
+	if err == nil {
+		return true, nil
+	}
+
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	return true, err
+}
+
+func writeProgressToFile(fileName string, blockHeight int64) error {
+	err := os.WriteFile(fileName, []byte(fmt.Sprintf("%d", blockHeight)), 0644)
+	if err != nil {
+		return fmt.Errorf("could not create initial progress file %v", err)
+	}
+	return nil
+}
+
+func readProgressFromFile(fileName string) (int64, error) {
+	dat, err := os.ReadFile(fileName)
+	if err != nil {
+		return 0, fmt.Errorf("ProgressFile is not valid %v", err)
+	}
+
+	stringValue := strings.TrimSpace(string(dat))
+
+	return strconv.ParseInt(stringValue, 10, 64)
+}
+
+func splitByWidthMake(str string, size int) []string {
+	strLength := len(str)
+	splitedLength := int(math.Ceil(float64(strLength) / float64(size)))
+	splited := make([]string, splitedLength)
+	var start, stop int
+	for i := 0; i < splitedLength; i += 1 {
+		start = i * size
+		stop = start + size
+		if stop > strLength {
+			stop = strLength
+		}
+		splited[i] = str[start:stop]
+	}
+	return splited
+}
+
+func fileAsImageData(path string) (string, error) {
+	f, _ := os.Open(path)
+
+	defer f.Close()
+
+	// Read entire JPG into byte slice.
+	reader := bufio.NewReader(f)
+	content, err := io.ReadAll(reader)
+	if err != nil {
+		return "", fmt.Errorf("could not read imageFile %s, %w", path, err)
+	}
+
+	return contentAsImageDataUrl(content), nil
+}
+
+func contentAsImageDataUrl(content []byte) string {
+	contentType := http.DetectContentType(content)
+
+	// Encode as base64.
+	encoded := base64.StdEncoding.EncodeToString(content)
+
+	return "data:" + contentType + ";base64, " + encoded
+}
+
+func fileAsBase64(path string) (string, error) {
+	f, _ := os.Open(path)
+
+	defer f.Close()
+
+	// Read entire JPG into byte slice.
+	reader := bufio.NewReader(f)
+	content, err := io.ReadAll(reader)
+	if err != nil {
+		return "", fmt.Errorf("could not read file %s, %w", path, err)
+	}
+
+	// Encode as base64.
+	encoded := base64.StdEncoding.EncodeToString(content)
+
+	return encoded, nil
+}
+
+func getUrl(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
+}
+
+func randomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, length)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)[:length]
+}
+
+// HexToAddress converts a hex string to an Address.
+func hexToAddress(h string) (*cadence.Address, error) {
+	trimmed := strings.TrimPrefix(h, "0x")
+	if len(trimmed)%2 == 1 {
+		trimmed = "0" + trimmed
+	}
+	b, err := hex.DecodeString(trimmed)
+	if err != nil {
+		return nil, err
+	}
+	address := cadence.BytesToAddress(b)
+	return &address, nil
+}
